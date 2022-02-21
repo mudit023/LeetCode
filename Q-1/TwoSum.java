de86b5ec8a85e7dcb866d9f8568dd0f7d8381c14
Mudit Mishra <Mudit.12016298@lpu.in>
2022-02-21 17:54:03 +0530

    Corrected two sum

--- Q-1/TwoSum.java
+++ Q-1/TwoSum.java
@@ -1,22 +1,19 @@
-public class TwoSum{
-  public static void main(String[] args){
-    
+public class TwoSum {
+  public static void main(String[] args) {
+
   }
 
-  public int[] twoSum(int[] nums, int target){
-    int n = 0;
-    int sum = 0;
-    int[] result = new int[n];
-    for(int i=0; i<nums.length; i++)
-    {
-      sum = sum + nums[i];
-      if (sum==target) {
-        n=i;
-        break;
-      } 
+  public int[] twoSum(int[] nums, int target) {
+    int[] res = new int[2];
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if ((nums[i] + nums[j]) == target) {
+          res[0] = i;
+          res[1] = j;
+          return res;
+        }
+      }
     }
-
-    
-    return result;
+    return null;
   }
 }
---
+++ Questions/src/ThreeSum.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ThreeSum {
+  public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    
+  }
+}
